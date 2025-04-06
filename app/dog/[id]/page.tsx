39c01eb4a37f93c6064cca3526120a489d0ae552
malpha-alt <matheus.alphadas@gmail.com>
2025-04-05 22:09:36 -0400

    baddasdasd

--- app/dog/[id]/page.tsx
+++ app/dog/[id]/page.tsx
@@ -1,13 +1,13 @@
 import { getDogById } from "../../../lib/getDogById";
 import DogDetail from "@/components/DogDetail";
 
-interface PageProps {
-  params: { id: string };
-}
+type Params = Promise<{
+  id: string
+}>;
 
-export default async function DogPage({ params }: PageProps) {
-  const { id } = params;
-  const breed = await getDogById(id);
+export default async function DogPage(props: { params: Params }) {
+    const params = await props.params;
+    const breed = await getDogById(params.id);
 
   return (
     <div className="w-[80%] mx-auto py-8">
